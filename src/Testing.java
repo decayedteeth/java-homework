85098d4fd10f36f0adca226c4d559276807ca396
cc <good@good.com>
2021-11-13 20:52:51 +0800

    test

--- src/Testing.java
+++ src/Testing.java
@@ -2,5 +2,6 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("testing");
         System.out.println("ing");
+        System.out.println("testing");
     }
 }
